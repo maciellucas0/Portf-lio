9f9c49ff1eca1fd8e998f5d4b940e90e21bd565e
Lucas Maciel <lucasm.contato@hotmail.com>
2023-01-20 15:46:57 -0300

    Inserção informações pessoais

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Lucas Maciel",
+  githubUser: "maciellucas0",
+  linkedinUser: "maciellucas",
+  whatsappNumber: "+5511966295759",
 };
