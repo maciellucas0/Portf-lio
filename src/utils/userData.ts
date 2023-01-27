800905963af5f778df5513cdd0823a8803b1fae1
unknown <lucasm.contato@hotmail.com>
2023-01-27 16:55:21 -0300

    Ajuste

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,5 +1,5 @@
 export const userData = {
-  nameUser: "Lucas Maciel",
+  nameUser: "Lucas Maciel ",
   githubUser: "maciellucas0",
   linkedinUser: "maciellucas",
   whatsappNumber: "+5511966295759",
